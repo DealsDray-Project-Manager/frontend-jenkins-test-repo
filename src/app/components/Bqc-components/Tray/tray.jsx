31de87714a63e3740cb6e823d6ce990240ff28af
Rafnas <mohd.rafnas@dealsdray.com>
2023-01-20 14:39:54 +0530

    done

--- src/app/components/Bqc-components/Tray/tray.jsx
+++ src/app/components/Bqc-components/Tray/tray.jsx
@@ -53,8 +53,8 @@ const SimpleMuiTable = () => {
             name: 'index',
             label: 'Record No',
             options: {
-                filter: true,
-                sort: true,
+                filter: false,
+                sort: false,
                 customBodyRender: (rowIndex, dataIndex) =>
                     dataIndex.rowIndex + 1,
             },
@@ -127,21 +127,21 @@ const SimpleMuiTable = () => {
             options: {
                 filter: true,
                 customBodyRender: (value, tableMeta) =>
-                    tableMeta.rowData[7].length == 0 &&
-                    tableMeta.rowData[8].length == 0
-                        ? value.length + '/' + tableMeta.rowData[6]
-                        : tableMeta.rowData[9] === 'BQC work inprogress'
-                        ? tableMeta.rowData[7].length +
-                          tableMeta.rowData[8].length +
-                          '/' +
-                          tableMeta.rowData[6]
-                        : tableMeta.rowData[7].length !== 0 ||
-                          tableMeta.rowData[8].length !== 0
-                        ? value.length +
-                          tableMeta.rowData[8].length +
+                    tableMeta.rowData[6].length == 0 &&
+                    tableMeta.rowData[7].length == 0
+                        ? value.length + '/' + tableMeta.rowData[5]
+                        : tableMeta.rowData[8] === 'BQC work inprogress'
+                        ? tableMeta.rowData[6].length +
                           tableMeta.rowData[7].length +
                           '/' +
-                          tableMeta.rowData[6]
+                          tableMeta.rowData[5]
+                        : tableMeta.rowData[6].length !== 0 ||
+                          tableMeta.rowData[7].length !== 0
+                        ? value.length +
+                          tableMeta.rowData[7].length +
+                          tableMeta.rowData[6].length +
+                          '/' +
+                          tableMeta.rowData[5]
                         : '',
             },
         },
@@ -185,7 +185,7 @@ const SimpleMuiTable = () => {
         <Container>
             <div className="breadcrumb">
                 <Breadcrumb
-                    routeSegments={[{ name: 'Bqc-Request', path: '/pages' }]}
+                    routeSegments={[{ name: 'Bqc-Request', path: '/' }]}
                 />
             </div>
 
@@ -203,6 +203,22 @@ const SimpleMuiTable = () => {
                     // print: false, // set print option
                     // pagination: true, //set pagination option
                     // viewColumns: false, // set column option
+                    customSort: (data, colIndex, order) => {
+                        return data.sort((a, b) => {
+                            if (colIndex === 1) {
+                                return (
+                                    (a.data[colIndex].price <
+                                    b.data[colIndex].price
+                                        ? -1
+                                        : 1) * (order === 'desc' ? 1 : -1)
+                                )
+                            }
+                            return (
+                                (a.data[colIndex] < b.data[colIndex] ? -1 : 1) *
+                                (order === 'desc' ? 1 : -1)
+                            )
+                        })
+                    },
                     elevation: 0,
                     rowsPerPageOptions: [10, 20, 40, 80, 100],
                 }}

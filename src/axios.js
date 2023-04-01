791d993d44a1b7fdbb9eeced66ade8ebc6af52f2
Rafnas <mohd.rafnas@dealsdray.com>
2023-04-01 14:21:24 +0530

    Updated on live

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const baseURL = 'http://localhost:8000'
-// const baseURL = 'http://prexo-v8-adminapi.dealsdray.com'
+// const baseURL = 'http://prexo-v8-testing-adminapi.dealsdray.com'
 
 
 

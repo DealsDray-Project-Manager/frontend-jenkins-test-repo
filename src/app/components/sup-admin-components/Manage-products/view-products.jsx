8a97cc3bc46b87cf610fd175cff6ba46fb439080
Rafnas <mohd.rafnas@dealsdray.com>
2023-06-02 11:10:01 +0530

    solution updated on live

--- src/app/components/sup-admin-components/Manage-products/view-products.jsx
+++ src/app/components/sup-admin-components/Manage-products/view-products.jsx
@@ -201,7 +201,7 @@ const SimpleMuiTable = () => {
                             width="80px"
                             src={
                                 value == undefined
-                                    ? 'https://prexo-v8-3-adminapi.dealsdray.com/product/image/' +
+                                    ? 'https://prexo-v8-3-dev-api.dealsdray.com/product/image/' +
                                       tableMeta.rowData[2] +
                                       '.jpg'
                                     : value

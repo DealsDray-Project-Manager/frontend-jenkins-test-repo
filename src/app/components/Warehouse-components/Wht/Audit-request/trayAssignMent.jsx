e566b5a1e001c12c2f59efde8b7f1a1e6b74c418
Rafnas <mohd.rafnas@dealsdray.com>
2023-09-19 09:52:04 +0530

    Bug fix done on rack-change and rack-id added in super-admin tray-master section

--- src/app/components/Warehouse-components/Wht/Audit-request/trayAssignMent.jsx
+++ src/app/components/Warehouse-components/Wht/Audit-request/trayAssignMent.jsx
@@ -41,27 +41,7 @@ const MemberEditorDialog = ({
     const [validationState, setValidationState] = useState(false)
     const [otherTrayAssign, setOtherTrayAssign] = useState([])
 
-    useEffect(() => {
-        const fetchCtxTray = async () => {
-            try {
-                const res = await axiosWarehouseIn.post(
-                    '/getCtxCategorysForIssue',
-                    alReadyIssuedTrayGrade
-                )
-                if (res.status === 200) {
-                    setCtxGrade(res?.data)
-                }
-            } catch (error) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: error,
-                })
-            }
-        }
-        fetchCtxTray()
-        return () => {}
-    }, [])
+   
     // HANDEL ADD TO STATE
     const handelAddTrayId = (e, grade, value) => {
         e.preventDefault()
